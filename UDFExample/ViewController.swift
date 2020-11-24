2f26ad3a8d68e666e4190618cf6d8f3bae865585
Anton Goncharov <anton.goncharov@mybuddy.ai>
2020-11-24 17:43:57 +0300

    Initial Commit

---
+++ UDFExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UDFExample
+//
+//  Created by Anton Goncharov on 24.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
